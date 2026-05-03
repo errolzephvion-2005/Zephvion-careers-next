4b1fd7a94c38486f6996f9759326ea4eb416f03e
AhmedTasleem <ahmedtasleem296699@gmail.com>
2026-05-03 19:22:36 +0530

    Build fix v1

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,48 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- allowedDevOrigins: ['192.168.56.1', '192.168.0.0/16'],
+  allowedDevOrigins: ['192.168.56.1', '192.168.0.0/16'],
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
     },
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
   },
 };
 
